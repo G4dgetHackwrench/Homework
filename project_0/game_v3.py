2e66271469f10fbcbf9ea512994b572a02bbac3c
Webbigail1 <anzel@list.ru>
2023-10-24 23:32:43 +0300

    edit readme, game3

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -28,36 +28,3 @@ def best_game(number: int = 1) -> int:
             predict = np.round((min_num + max_num) /2)
 
     return count
-
-
-def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
-
-    Args:
-        random_predict ([type]): функция угадывания
-
-    Returns:
-        int: среднее количество попыток
-    """
-    count_ls = []
-    #np.random.seed(1)  # Фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # Загадали список чисел
-
-    for number in random_array:
-        count_ls.append(random_predict(number))
-
-    score = int(np.mean(count_ls))
-    
-    if str(score)[-1] == '1':
-        print(f'Ваш алгоритм угадывает число в среднем за: {score} попытку')
-    
-    elif str(score)[-1] in ['2', '3', '4']:
-        print(f'Ваш алгоритм угадывает число в среднем за: {score} попытки')
-        
-    else:
-        print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
-        
-    return(score)
-    
-    
-score_game(best_game)
